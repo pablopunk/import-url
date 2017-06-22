5c1f435aacdd27c88e9dd39cfc84ff4c9a3a58ae
Pablo Varela <pablovarela182@gmail.com>
2017-06-22 18:02:14 -0400

    Better test usage

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 const test = require('ava')
-const r = require('.')
 
 const simpleModuleUrl = 'https://raw.githubusercontent.com/sindresorhus/semver-regex/master/index.js'
 
 test('Require sample module', async t => {
   t.plan(1)
-  const simpleModule = await r(simpleModuleUrl)
+  const simpleModule = await require('.')(simpleModuleUrl)
   t.is(typeof simpleModule, 'function') 
 })
