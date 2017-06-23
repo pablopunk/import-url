9545dabb351396e4d6fac60c04182660ef8312e1
Pablo Varela <pablovarela182@gmail.com>
2017-06-22 21:07:21 -0400

    Making the code unreadable because I can

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const got = require('got')
-const requireString = require('require-from-string')
-
-module.exports = async url => {
-  const response = await got(url)
-  return requireString(response.body)
-}
+module.exports = async url => require('require-from-string')((await require('got')(url)).body)
